0a797e7334c46301ada48d06ed005f3c100bf3d9
Yuzu02 <57969791+Yuzu02@users.noreply.github.com>
2023-11-04 21:27:51 -0400

    pass 4st stage

--- app/main.py
+++ app/main.py
@@ -44,19 +44,21 @@ class Request:
     """A class representing a HTTP request."""
 
     def __init__(
-        self, http_method: HttpMethod = None, path: str = None, http_version: str = None
-    ) -> None:
-        self.http_method = http_method
-        self.path = path
+        self, http_version: str, http_status_code: HttpStatusCode, body: str = None
+     ) -> None:
         self.http_version = http_version
+        self.http_status_code = http_status_code
+        self.body = body
+        self.content_type = "text/plain"
+        self.content_length = 0 if self.body is None else len(self.body)
 
-    def decode(self, data: bytes):
-        """Parse the data from the client into a Request object."""
-        data_list = data.decode().split(CRLF)
-        http_method, path, http_version = data_list[0].split()
-        self.http_method = HttpMethod(http_method)
-        self.path = path
-        self.http_version = http_version
+    def encode(self) -> bytes:
+        """Encode the response into bytes."""
+        message = f"{self.http_version} {self.http_status_code.code} {self.http_status_code.message}{CRLF}"
+        message += f"Content-Type: {self.content_type}{CRLF}"
+        message += f"Content-Length: {self.content_length}{END_HEADERS}"
+        message += f"{self.body}"
+        return message.encode()
 
 
 def main():
@@ -79,9 +81,12 @@ def main():
         # Create a Response object
         if request.path == "/":
             response = Response(request.http_version, HttpStatusCode.OK)
+            
+        elif request.path.startswith("/echo/"):
+            message = request.path.split("/echo/")[1]
+            response = Response(request.http_version, HttpStatusCode.OK, message)
         else:
             response = Response(request.http_version, HttpStatusCode.NOT_FOUND)
-
         # Send the response to client
         client_socket.sendall(response.encode())
 
