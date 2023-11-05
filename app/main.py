125ea9bc31fe8292146e86f8c53f46a2cf09b849
Yuzu02 <57969791+Yuzu02@users.noreply.github.com>
2023-11-04 21:45:44 -0400

    pass 4st stage

--- app/main.py
+++ app/main.py
@@ -1,94 +1,50 @@
-"""A simple HTTP server."""
 import socket
-from enum import StrEnum, Enum
 
-# Constants
-HOST = "localhost"
-PORT = 4221
-BUFFER_ZISE = 1024
-CRLF = "\r\n"
-END_HEADERS = CRLF + CRLF
+HOST, PORT = "localhost", 4221
 
-
-class HttpMethod(StrEnum):
-    """An enum representing the HTTP methods."""
-
-    GET = "GET"
+code_status = {
+    200: 'OK',
+    404: 'Not Found',
+}
 
 
-class HttpStatusCode(Enum):
-    """An enum representing the HTTP status codes."""
-
-    OK = (200, "OK")
-    NOT_FOUND = (404, "Not Found")
-
-    def __init__(self, code: int, message: str) -> None:
-        self.code = code
-        self.message = message
-
-
-class Response:
-    """A class representing a HTTP response."""
-
-    def __init__(self, http_version: str, http_status_code: HttpStatusCode) -> None:
-        self.http_version = http_version
-        self.http_status_code = http_status_code
-
-    def encode(self) -> bytes:
-        """Encode the response into bytes."""
-        message = f"{self.http_version} {self.http_status_code.code} {self.http_status_code.message}{END_HEADERS}"
-        return message.encode()
-
-
-class Request:
-    """A class representing a HTTP request."""
-
-    def __init__(
-        self, http_method: HttpMethod = None, path: str = None, http_version: str = None
-    ) -> None:
-        self.http_method = http_method
-        self.path = path
-        self.http_version = http_version
-
-    def decode(self, data: bytes):
-        """Parse the data from the client into a Request object."""
-        data_list = data.decode().split(CRLF)
-        http_method, path, http_version = data_list[0].split()
-        self.http_method = HttpMethod(http_method)
-        self.path = path
-        self.http_version = http_version
+def parsed_response(code: int, headers: dict = {}, body: str = "") -> str:
+    response_headers = f"HTTP/1.1 {code} {code_status.get(code)}\r\n"
+    for key in headers:
+        response_headers += f"{key}: {headers[key]}\r\n"
+    response_headers += "\r\n"
+    return response_headers + body
 
 
 def main():
-    """The main function."""
+    # Dev
+    # server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # server.bind((HOST, PORT))
+    # server.listen(5)
 
-    # Create a TCP socket
-    server_socket = socket.create_server((HOST, PORT))
+    # Deploy
+    server = socket.create_server(("localhost", 4221), reuse_port=True)
 
-    # Accept the connection from TCP client
-    client_socket, client_address = server_socket.accept()
+    print("Server in port:", PORT)
+    conn, address = server.accept()
+    print("Connected by:", address)
 
-    with client_socket:
-        # Receive the data from client
-        data = client_socket.recv(BUFFER_ZISE)
+    data = conn.recv(1024).decode().splitlines()
+    print(data[0])
 
-        # Parse the data into a Request object
-        request = Request()
-        request.decode(data)
-
-        # Create a Response object
-        if request.path == "/":
-            response = Response(request.http_version, HttpStatusCode.OK)
-        elif request.path.startswith("/echo/"):
-            message = request.path.split("/echo/")[1]
-            response = Response(request.http_version, HttpStatusCode.OK, message)
-        else:
-            response = Response(request.http_version, HttpStatusCode.NOT_FOUND)
-        # Send the response to client
-        client_socket.sendall(response.encode())
-
-    # Close the connection
-    server_socket.close()
+    http_status = data[0].split(' ')
+    path = http_status[1]
+    if http_status[1] == '/':
+        response = parsed_response(200)
+    elif path.startswith('/echo/'):
+        message = path.split('/echo/')[1]
+        response = parsed_response(200, {
+            "Content-Type":  "text/plain",
+            "Content-Length": f"{len(message)}"
+        }, f"{message}")
+    else:
+        response = parsed_response(404)
+    conn.sendall(response.encode())
 
 
 if __name__ == "__main__":
