eca71f86c77e4ad8ec9dab6cca3b902e2668271b
Yuzu02 <57969791+Yuzu02@users.noreply.github.com>
2023-11-04 21:55:44 -0400

    pass 6st stage

--- app/main.py
+++ app/main.py
@@ -1,105 +1,129 @@
 """A simple HTTP server."""
 import socket
-from enum import StrEnum, Enum
+import threading
+from enum import Enum
 
-# Constants
 HOST = "localhost"
 PORT = 4221
 BUFFER_ZISE = 1024
-CRLF = "\r\n"
+CRLF = '\r\n'
 END_HEADERS = CRLF + CRLF
 
 
-class HttpMethod(StrEnum):
-    """An enum representing the HTTP methods."""
-
+class HttpMethod(Enum):
+    """An enum representing HTTP methods."""
     GET = "GET"
 
 
 class HttpStatusCode(Enum):
-    """An enum representing the HTTP status codes."""
 
     OK = (200, "OK")
     NOT_FOUND = (404, "Not Found")
 
-    def __init__(self, code: int, message: str) -> None:
-        self.code = code
-        self.message = message
 
-
-class Response:
-    """A class representing a HTTP response."""
-
-    def __init__(
-        self, http_version: str, http_status_code: HttpStatusCode, body: str = None
-    ) -> None:
+class RequestLine:
+    def __init__(self, method: HttpMethod, path: str, http_version: str) -> None:
+        self.method = method
+        self.path = path
         self.http_version = http_version
-        self.http_status_code = http_status_code
-        self.body = body
-        self.content_type = "text/plain"
-        self.content_length = 0 if self.body is None else len(self.body)
-
-    def encode(self) -> bytes:
-        """Encode the response into bytes."""
-        message = f"{self.http_version} {self.http_status_code.code} {self.http_status_code.message}{CRLF}"
-        message += f"Content-Type: {self.content_type}{CRLF}"
-        message += f"Content-Length: {self.content_length}{END_HEADERS}"
-        message += f"{self.body}"
-        return message.encode()
 
 
 class Request:
-    """A class representing a HTTP request."""
+    def __init__(self, data: bytes) -> None:
+        data: list[str] = data.decode().splitlines().copy()
+        method, path, http_version = data[0].split(' ')
+        data.pop(0)
+        self.method = HttpMethod(method)
+        self.path = path
+        self.http_version = http_version
+        self.headers = {}
+        for header in data:
+            if header == '':
+                break
+            key, value = header.split(': ')
+            self.headers[key] = value
 
+    def get_request_line(self) -> RequestLine:
+        return RequestLine(self.method, self.path, self.http_version)
+
+
+class Response:
     def __init__(
-        self, http_method: HttpMethod = None, path: str = None, http_version: str = None
+        self,
+        request_line: RequestLine,
+        code: HttpStatusCode,
+        headers: dict = {},
+        body: str = ""
     ) -> None:
-        self.http_method = http_method
-        self.path = path
-        self.http_version = http_version
-        self.user_agent = None
+        self.rq = request_line
+        self.code = code
+        self.headers = headers
+        self.body = body
 
-    def decode(self, data: bytes):
-        """Parse the data from the client into a Request object."""
-        data_list = data.decode().split(CRLF)
-        http_method, path, http_version = data_list[0].split()
-        user_agent = data_list[2].split(":")[1].strip()
-        self.http_method = HttpMethod(http_method)
-        self.path = path
-        self.http_version = http_version
-        self.user_agent = user_agent
+    def encode(self, ):
+        message = f"{self.rq.http_version} {self.code.value[0]} {self.code.value[1]}"
+        for header in self.headers:
+            message += f"{CRLF}{header}: {self.headers[header]}"
+        message += END_HEADERS + self.body
+        return message.encode()
+
+
+def client_handler(conn):
+    data = conn.recv(BUFFER_ZISE)
+    request = Request(data)
+
+    if request.path == '/':
+        response = Response(request.get_request_line(), HttpStatusCode.OK)
+    elif request.path.startswith('/echo/'):
+        message = request.path.split('/echo/')[1]
+        response = Response(
+            request.get_request_line(),
+            HttpStatusCode.OK,
+            headers={
+                'Content-Type': "text/plain",
+                'Content-Length': len(message),
+            },
+            body=message
+        )
+    elif request.path == '/user-agent':
+        message = request.headers.get('User-Agent')
+        response = Response(
+            request.get_request_line(),
+            HttpStatusCode.OK,
+            headers={
+                'Content-Type': "text/plain",
+                'Content-Length': len(message),
+            },
+            body=message
+        )
+    else:
+        response = Response(
+            request.get_request_line(),
+            HttpStatusCode.NOT_FOUND
+        )
+
+    conn.sendall(response.encode())
+    conn.close()
 
 
 def main():
-    """The main function."""
+    # Dev
+    # server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # server.bind((HOST, PORT))
 
-    # Create a TCP socket
-    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
+    # Deploy
+    server = socket.create_server(("localhost", 4221), reuse_port=True)
+    server.listen()
+    print("Server in port:", PORT)
 
-    # Accept the connection from TCP client
-    client_socket, client_address = server_socket.accept()
-
-    with client_socket:
-        # Receive the data from client
-        data = client_socket.recv(BUFFER_ZISE)
-
-        # Parse the data into a Request object
-        request = Request()
-        request.decode(data)
-
-        # Create a Response object
-        if request.path == "/":
-            response = Response(request.http_version, HttpStatusCode.OK)
-        elif request.path.startswith("/echo/"):
-            message = request.path.split("/echo/")[1]
-            response = Response(request.http_version, HttpStatusCode.OK, message)
-        elif request.path.startswith("/user-agent"):
-            response = Response(request.http_version, HttpStatusCode.OK, request.user_agent)
-        else:
-            response = Response(request.http_version, HttpStatusCode.NOT_FOUND)
-
-        # Send the response to client
-        client_socket.sendall(response.encode())
+    while True:
+        conn, address = server.accept()
+        print("Connected by:", address)
+        thread = threading.Thread(
+            target=client_handler,
+            args=(conn,)
+        )
+        thread.start()
 
 
 if __name__ == "__main__":
